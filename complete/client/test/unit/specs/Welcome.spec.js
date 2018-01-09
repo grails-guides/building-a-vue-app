2b421acaa353e035efa2a60d4e5b810a042dc5ee
Zachary Klein <zak@silver-chalice.com>
2018-01-09 16:50:26 -0600

    Updated complete project

---
+++ complete/client/test/unit/specs/Welcome.spec.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Welcome from '@/components/Welcome'
+const uiv = require("uiv");
+
+
+describe('Welcome.vue', () => {
+  it('should render correct contents', () => {
+    Vue.use(uiv)
+    const Constructor = Vue.extend(Welcome)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('#content .row h1').textContent)
+      .toEqual('Welcome to Grails')
+  })
+})
